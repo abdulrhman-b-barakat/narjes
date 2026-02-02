576fde54c6b2fc621ba23e71eee75abeb95f231a
abdulrhman-b-barakat <abdulrhman.b.barakat@gmail.com>
2026-02-02 10:00:00 +0300

    feat: initial project setup and asset configuration for Narjes

---
+++ src/constants/index.ts
@@ -0,0 +1,45 @@
+// Header Links
+export const CATEGORIES = [
+  { name: "Men", slug: "/products?gender=Men" },
+  { name: "Women", slug: "/products?gender=women" },
+  { name: "Top wear", slug: "/products?category=Top+Wear" },
+  { name: "Bottom wear", slug: "/products?category=Bottom+Wear" },
+];
+
+
+//  Search Filters
+export const CATEGORIES_PRODUCTS = ["Top Wear", "Bottom Wear"];
+export const GENDERS = ["Men", "Women"];
+export const COLORS = [
+  "Red",
+  "Blue",
+  "Black",
+  "Green",
+  "Yellow",
+  "Gray",
+  "White",
+  "Pink",
+  "Beige",
+  "Navy",
+];
+export const MATERIALS = [
+  "Cotton",
+  "Wool",
+  "Denim",
+  "Polyester",
+  "Silk",
+  "Linen",
+  "Viscose",
+  "Fleece",
+];
+export const BRANDS = [
+  "Urban Threads",
+  "Modern Fit",
+  "Street Style",
+  "Beach Breeze",
+  "Fashionista",
+  "ChicStyle",
+];
+export const SIZES = ["XS", "S", "M", "L", "XL", "XXL"];
+
+
